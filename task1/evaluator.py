f7e7374f43a2e17c401bec1c78ab4e91b89d6154
Ivan Eggel <ivan.eggel@gmail.com>
2023-03-28 11:39:31 +0200

    Finalize first version

--- task1/evaluator.py
+++ task1/evaluator.py
@@ -6,59 +6,70 @@ import os
 class ImageCLEFGansTask1Validator:
     def __init__(self, image_ids_file_path):
         self.image_ids_file_path = image_ids_file_path
-        self.load_image_ids();
+        self._load_image_ids()
 
-    def load_image_ids(self):
+    def _load_image_ids(self):
         with open(self.image_ids_file_path) as file:
             self.image_ids = [line.rstrip() for line in file]
 
     def validate_format(self, submission_file_path):
         print("validating format...")
-        
+
         with open(submission_file_path) as csv_file:
-            reader = csv.reader(csv_file, skipinitialspace=True, delimiter=',', quoting=csv.QUOTE_NONE)
+            reader = csv.reader(csv_file, delimiter=',',
+                                quoting=csv.QUOTE_NONE)
             line_nbr = 0
             predictions = {}
 
             for row in reader:
                 line_nbr += 1
 
+                # Not 2 comma separated tokens on line => Error
                 if len(row) != 2:
-                    self.raise_exception("Wrong format: Each line must consist of 2 tokens separated by a comma. You have to specify a figure ID "
-                                            + "followed by a score of 0 or 1 to indicates whether the image was used for training or not (0=not used, 1=used). "
+                    self._raise_exception("Wrong format: Each line must consist of 2 tokens separated by a comma. You have to specify a figure ID "
+                                          + "followed by a score of 0 or 1 to indicates whether the image was used for training or not (0=not used, 1=used). "
                                             + "Each token must be separated by a comma ({}).", line_nbr, "<figure_id>, <0 | 1>")
 
-                image_id = row[0]
+                image_id = row[0].strip()
 
+                # Figure ID not contained in testset => Error
                 if image_id not in self.image_ids:
-                    self.raise_exception("Figure ID '{}' is not contained in testset.", line_nbr, image_id)
+                    self._raise_exception(
+                        "Figure ID '{}' is not contained in testset.", line_nbr, image_id)
 
+                # Figure ID contained more then once => Error
                 if image_id in predictions:
-                    self.raise_exception("Figure ID '{}' was specified more than once in submission file.", line_nbr, image_id)         
-                    
-                score = row[1]
-                
+                    self._raise_exception(
+                        "Figure ID '{}' was specified more than once in submission file.", line_nbr, image_id)
+
+                score = row[1].strip()
+
+                # Score not 0 or 1 => Error
                 try:
                     score = int(score)
-                    print(score)
-                    if score != 0 and score !=1:
+                    if score != 0 and score != 1:
                         raise ValueError
                 except ValueError:
-                    self.raise_exception("Score '{}' must be either 0 or 1 (0=not used for training, 1=used for training).", line_nbr, score)
+                    self._raise_exception(
+                        "Wrong score '{}'. Score must be either 0 or 1 (0=not used for training, 1=used for training).", line_nbr, score)
 
                 predictions[image_id] = score
 
-
+            # Not all figure IDs included => Error
             if len(predictions) != len(self.image_ids):
-                self.raise_exception("Number of figure IDs in submission file not equal to number of figure IDs in testset.", line_nbr)
+                self._raise_exception(
+                    "Number of figure IDs in submission file not equal to number of figure IDs in testset.", line_nbr)
+
+            print("The format os your submission file '{}' is correct. Good Job!".format(
+                submission_file_path))
+
+    def _raise_exception(self, message, line_nbr, *args):
+        raise SubmissionValidationException(message.format(*args) +
+                                            " Error occured at line nbr {}.".format(line_nbr))
 
 
-
-    def raise_exception(self, message, line_nbr, *args):
-        raise Exception(message.format(*args) +
-                        " Error occured at line nbr {}.".format(line_nbr))
-
-
+class SubmissionValidationException(Exception):
+    pass
 
 
 if __name__ == "__main__":
@@ -66,8 +77,8 @@ if __name__ == "__main__":
     usage_message = "Usage: evaluator.py submission_file_path"
 
     if len(sys.argv) != 2 or (len(sys.argv) == 2 and str(sys.argv[1]) == '--help'):
-       print(usage_message)
-       sys.exit()
+        print(usage_message)
+        sys.exit()
 
     submission_file_path = str(sys.argv[1])
 
@@ -75,8 +86,12 @@ if __name__ == "__main__":
         print("{} is not an existing file".format(submission_file_path))
         sys.exit()
 
-    image_ids_file_path = "./image_ids"
-    
+    image_ids_file_path = "./resources/image_ids.txt"
 
-    imageCLEFGansTask1Validator = ImageCLEFGansTask1Validator(image_ids_file_path)
-    imageCLEFGansTask1Validator.validate_format(submission_file_path)
+    imageCLEFGansTask1Validator = ImageCLEFGansTask1Validator(
+        image_ids_file_path)
+
+    try:
+        imageCLEFGansTask1Validator.validate_format(submission_file_path)
+    except SubmissionValidationException as ex:
+        print(ex)
